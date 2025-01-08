7732ef3e8520761f6ffc2a47c619bac189c9a2ca
AYUSH AWASTHI🚀 <97173127+AyushAwasthi2384@users.noreply.github.com>
2025-01-08 22:21:21 +0530

    db link

--- Database/todo.database.js
+++ Database/todo.database.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const mongoURI = "mongodb://127.0.0.1:27017/todolist";
+const mongoURI = process.env.DB_URL;
 const connectDB = () => {
   mongoose
     .connect(mongoURI)
